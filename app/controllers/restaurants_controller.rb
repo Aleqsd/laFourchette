7535d12052752c216eee22d643b3dc5126928185
Aleqsd <aleqsd@gmail.com>
2017-04-18 11:22:23 +0200

    Added new form & create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,4 +5,27 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.valid?
+      @restaurant.save!
+      redirect_to restaurant_path(@restaurant)
+    else
+      # réaffiche le template new pour avoir formulaire + erreurs
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    # n'autorise que ces paramètres
+    params.require(:restaurant).permit(:name, :photo, :city)
+  end
+
 end
